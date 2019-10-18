dff05fd964960a2a99f1135a02b343a0f88b14b7
RMLoureiro <rf.loureiro@campus.fct.unl.pt>
2019-10-18 14:54:21 +0100

    command interperter

--- scc-frontend/src/frontend/Database/Frontend.java
+++ scc-frontend/src/frontend/Database/Frontend.java
@@ -3,6 +3,10 @@ package frontend.Database;
 import com.microsoft.azure.cosmosdb.*;
 import com.microsoft.azure.cosmosdb.rx.AsyncDocumentClient;
 import io.reactivex.netty.client.RxClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.Date;
 
@@ -24,18 +28,49 @@ public class Frontend {
     private static DatabaseConnector dbconnector;
     private static AsyncDocumentClient client;
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         dbconnector = new DatabaseConnector();
         client = dbconnector.getDocumentClient();
+
+
+        String line;
+        while(true){
+            line = reader.readLine();
+            String[] command = line.split("\\s+");
+            switch(command[0]){
+                case "initialPage":
+                    initialPage();
+                    break;
+                case "thread":
+                    getThread(command[1]);
+                    break;
+                case "like":
+                    likePost(command[1]);
+                    break;
+
+            }
+        }
     }
 
-    public void initialPage(){
+    private static void initialPage(){
         // as this is a multi collection enable cross partition query
         FeedOptions options = new FeedOptions();
         options.setEnableCrossPartitionQuery(true);
 
-        String collectionLink = dbconnector.getCollectionString("Post");
+        String collectionLink = dbconnector.getCollectionString("Posts");
 
-        Iterator<FeedResponse<Document>> it = client.queryDocuments(collectionLink, "SELECT * FROM Family WHERE Family.lastName = 'Andersen'", options).toBlocking().getIterator();
+        Iterator<FeedResponse<Document>> it = client.queryDocuments(collectionLink, "SELECT * FROM Posts", options).toBlocking().getIterator();
+
+        //TODO do something with the posts
+
+    }
+
+    public static void getThread(String id){
+
+    }
+
+    public static void likePost(String id){
+
     }
 }
