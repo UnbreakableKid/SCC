1f79518077a84efc706e01e1388f93184ef5c50a
João Pacheco <dblamk@hotmail.com>
2019-11-16 13:05:34 +0000

    community page

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SCC
+
+## TODO
+```
+Have to finish the professors tests
+Have to implement cache
+Have to create a function to redo initial page 
+```
---
+++ scc-backend/src/scc/pages/CommunityPage.java
@@ -0,0 +1,54 @@
+package scc.pages;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.microsoft.azure.cosmosdb.*;
+import com.microsoft.azure.cosmosdb.rx.AsyncDocumentClient;
+
+import resources.Database.DatabaseConnector;
+import resources.Database.entities.Posts;
+
+
+@Path("/community/{name}")
+public class CommunityPage {
+
+    private DatabaseConnector db = new DatabaseConnector();
+    private AsyncDocumentClient client = db.getDocumentClient();
+    private String UsersCollection = db.getCollectionString("Posts");
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public List<Posts> getUsers(@PathParam("name") String name){
+
+		List<Posts> z = new ArrayList<>();
+
+        FeedOptions queryOptions = new FeedOptions();
+        queryOptions.setEnableCrossPartitionQuery(true);
+        queryOptions.setMaxDegreeOfParallelism(-1);
+
+        Iterator<FeedResponse<Document>> it = client.queryDocuments(
+                UsersCollection, String.format("SELECT * FROM Posts p WHERE p.community.name = '%s'", name),
+                queryOptions).toBlocking().getIterator();
+        
+
+        	while( it.hasNext())
+			for( Document d : it.next().getResults()) {
+				System.out.println( d.toJson());
+				Gson g = new Gson();
+				Posts u = g.fromJson(d.toJson(), Posts.class);
+                z.add(u);
+			}
+
+        return z;
+    }
+}
