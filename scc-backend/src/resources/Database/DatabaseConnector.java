a7a6a091a5d0bc30f987925a51ab889972de2a6e
Joao Pacheco <dblamk@hotmail.com>
2019-12-05 14:01:10 +0000

    Done phase 2

--- scc-backend/src/resources/Database/DatabaseConnector.java
+++ scc-backend/src/resources/Database/DatabaseConnector.java
@@ -1,6 +1,5 @@
 package resources.Database;
 
-import java.util.ResourceBundle;
 
 import com.microsoft.azure.cosmosdb.ConnectionPolicy;
 import com.microsoft.azure.cosmosdb.ConsistencyLevel;
@@ -8,27 +7,25 @@ import com.microsoft.azure.cosmosdb.rx.AsyncDocumentClient;
 
 public class DatabaseConnector {
     
-    private static ResourceBundle rb = ResourceBundle.getBundle("resources.config");
-
  
    	private static AsyncDocumentClient client;
 
     public synchronized AsyncDocumentClient getDocumentClient() {
         if (client == null) {
             ConnectionPolicy connectionPolicy = ConnectionPolicy.GetDefault();
-            client = new AsyncDocumentClient.Builder().withServiceEndpoint(rb.getString("db.endpoint"))
-                    .withMasterKeyOrResourceToken(rb.getString("db.masterkey")).withConnectionPolicy(connectionPolicy)
+            client = new AsyncDocumentClient.Builder().withServiceEndpoint("https://scc1920-56982.documents.azure.com:443/")
+                    .withMasterKeyOrResourceToken("O7aHvn3kz9CkhVja4NmMFKl34nvXvsFfzYaJl9cUq00rU7rEMD7F3i70x7xG2joRq67Hi3axNJbzLbqDIr4C2g==").withConnectionPolicy(connectionPolicy)
                     .withConsistencyLevel(ConsistencyLevel.Session).build();
         }
         return client;
     }
 
     static String getDatabaseString() {
-		return String.format("/dbs/%s", rb.getString("db.database"));
+		return String.format("/dbs/%s","SCC-56982");
 	}
     
     public String getCollectionString(String col) {
-        return String.format("/dbs/%s/colls/%s", rb.getString("db.database"), col);
+        return String.format("/dbs/%s/colls/%s","SCC-56982", col);
     }
     
 }
